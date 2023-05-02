a7b7f5d00e2837e797eb23d5873df97f2432f193
furmu <nabila.furmmuly@jobtrek.ch>
2023-05-02 14:06:38 +0200

    "Centred signIn and sigUp buttons"

--- src/app/exercice/exercice.component.ts
+++ src/app/exercice/exercice.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-exercice',
   templateUrl: './exercice.component.html',
   styleUrls: ['./exercice.component.scss']
 })
+
+
+
 export class ExerciceComponent {
 
 }
