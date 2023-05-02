2b0ef47a07a73f960f59775f2a8547f7d99cf94b
candas <hrisohest@gmail.com>
2023-05-02 15:00:50 +0200

    header moved into app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        'transform': 'translate3d(0, 0, 0)',
+        'visibility': 'visible'
+      })),
+      state('out', style({
+        'transform': 'translate3d(100%, 0, 0)',
+        'visibility': 'hidden'
+      })),
+      transition('in => out', animate('400ms ease-out')),
+      transition('out => in', animate('400ms ease-in'))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'polymorFIT';
