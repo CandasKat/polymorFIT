2e90a70ca2ba6dbf13aafa56a216de878e200495
Candas Kat <hrisohest@gmail.com>
2023-05-21 12:48:37 +0200

    fix "remember me" checkbox

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {LoginService} from "./login.service";
@@ -8,11 +8,12 @@ import {LoginService} from "./login.service";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent
-{
+export class LoginComponent implements OnInit{
   mail = "";
   password = "";
   hide: boolean = true;
+  errorMessage: string | null = null;
+  rememberMeChecked: boolean = false;
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   passwordFormControl = new FormControl('', [Validators.required, Validators.required]);
   @Output() close = new EventEmitter();
@@ -20,19 +21,29 @@ export class LoginComponent
   cancel() {
     this.router.navigate(['/home']);
   }
+
+  ngOnInit() {
+    const rememberedUser = localStorage.getItem('rememberMe');
+    if (rememberedUser) {
+      this.mail = rememberedUser;
+    }
+  }
+
   onSubmit(): void {
+    this.errorMessage = null;
     this.loginService.loginSer(this.mail, this.password).subscribe(result => {
     if (result) {
-
-      console.log(result);
+      if (this.rememberMeChecked) {
+        localStorage.setItem('rememberMe', this.mail);
+      }
       this.router.navigate(["/home"]);
       } else {
-
-
+      this.errorMessage = "Incorrect username or password";
       }
     } ,
     error => {
       console.error('Error:', error);
+      this.errorMessage = "Something went wrong. Please try again later.";
     });
   }
 }
