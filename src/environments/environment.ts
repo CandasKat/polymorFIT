14057f817038fd1fd31dac9701db2cd85a4ea57b
Candas Kat <88893818+CandasKat@users.noreply.github.com>
2023-05-22 12:47:07 +0200

    Update environment.ts
    fix environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  GOOGLE_API_KEY: '724653378617-00kp4ave75skimric5bvqn3o3rr66pto.apps.googleusercontent.com',
-  NINJAS_EXERCISES_API_KEY: '6UU8hcZxFXHN4AVnSsOUHg==mTo54yTCui6EhkCa',
-  EXERCISESDB_API_KEY: '0b6df8825fmshf3d59c26eacae31p1c8113jsn3da9280aca87',
-  YOUTUBE_VIDEOS_API_KEY: '0b6df8825fmshf3d59c26eacae31p1c8113jsn3da9280aca87'
+  GOOGLE_API_KEY: '************',
+  NINJAS_EXERCISES_API_KEY: '************',
+  EXERCISESDB_API_KEY: '************',
+  YOUTUBE_VIDEOS_API_KEY: '************'
 };
